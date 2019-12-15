5c37bcfe9da4ea12b098e037a5a5535ee7c46ff7
Sergey Bashilov <sbashilov@ozon.ru>
2019-12-15 17:58:57 +0300

    simple workerpool

--- wp.go
+++ wp.go
@@ -2,52 +2,43 @@ package workerpool
 
 import "sync/atomic"
 
+// WP worker pool structure
 type WP struct {
-	workers        int64
-	taskQueue      chan func()
-	stopController chan struct{}
+	workers   int64
+	taskQueue chan func()
 }
 
+// New creates new worker pool
 func New() *WP {
-	wp := &WP{
-		taskQueue:      make(chan func()),
-		stopController: make(chan struct{}),
-	}
-	return wp
+	return &WP{taskQueue: make(chan func())}
 }
 
+// Submit put task to process queue
 func (wp *WP) Submit(task func()) {
 	if task != nil {
 		wp.taskQueue <- task
 	}
 }
 
+// Stop stops all workers
 func (wp *WP) Stop() {
-
+	close(wp.taskQueue)
 }
 
+// Dispatch runs provided number of workers
 func (wp *WP) Dispatch(workers int) {
 	for i := 1; i < workers; i++ {
-		atomic.AddInt64(&wp.workers, 1)
+		wp.workers++
 		go func() {
-		LOOP:
-			for {
-				select {
-				case <-wp.stopController:
-					break LOOP
-				default:
-					select {
-					case task, ok := <-wp.taskQueue:
-						if !ok {
-							break LOOP
-						}
-						if task != nil {
-							task()
-						}
-					default:
-					}
-				}
+			for task := range wp.taskQueue {
+				task()
 			}
+			atomic.AddInt64(&wp.workers, -1)
 		}()
 	}
+}
+
+// Workers returns count of runnable workers
+func (wp *WP) Workers() int64 {
+	return wp.workers
 }
